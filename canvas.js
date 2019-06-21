4078ec0f005cd9b5b3abb4568f5d9fc4afd91dce
Kiuuk <kiwook.ha@gmail.com>
2019-06-21 17:49:43 +0900

    set jerseys object

--- canvas.js
+++ canvas.js
@@ -1,5 +1,7 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvasFront = document.getElementById('canvasFront');
+const canvasBack = document.getElementById('canvasBack');
+const ctxFront = canvasFront.getContext('2d');
+const ctxBack = canvasBack.getContext('2d');
 
 const inputName = document.getElementById('inputName');
 const inputNumber = document.getElementById('inputNumber');
@@ -7,28 +9,40 @@ const displayName = document.getElementById('displayName');
 const displayNumber = document.getElementById('displayNumber');
 const submitBtn = document.getElementById('submit');
 
+const jerseys = {
+  front : 'img/front.png',
+  back : 'img/back.png'
+};
+
 function drawLogo() {
   let image = new Image();
   image.src = 'img/england-football-seeklogo.com.svg';
   image.onload = function(){
-    ctx.drawImage(this, 550, 500, 20, 25);
+    ctxBack.drawImage(this, 550, 500, 20, 25);
   };
+}
+
+function drawBack(backName, backNumber) {
+  displayName.textContent = backName;
+  displayNumber.textContent = backNumber;
 }
 
 submitBtn.addEventListener('click', function(e){
   e.preventDefault();
-  let displayName = inputName.value;
-  let displayNumber = inputNumber.value;
-  // drawTextNumber(displayNumber);
-  drawLogo();
+  let backName = inputName.value;
+  let backNumber = inputNumber.value;
+  drawBack(backName, backNumber);
+  // drawLogo();
 })
 
 window.addEventListener('DOMContentLoaded', function(){
-  let image = new Image();
-  image.src = 'img/back.png';
-  image.onload = function(){
-    canvas.width = this.naturalWidth;
-    canvas.height = this.naturalHeight;
-    ctx.drawImage(this, 0, 0);
-  };
+  let jerseyView = ['front', 'back'];
+
+  for (let i = 0; i < jerseyView.length; i++) {
+    let image = new Image();
+    image.src = jerseys[jerseyView[i]];
+    image.onload = function() {
+      jerseyView[i] === 'front' ? ctxFront.drawImage(this, 0, 0) : ctxBack.drawImage(this, 0, 0);
+    }
+  }
 })
